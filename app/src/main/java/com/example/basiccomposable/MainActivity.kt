2324f9c948baf8e2e0b15508f51889d0a2c8eb3f
ulfhiraa <studyiraa@gmail.com>
2024-10-16 17:39:15 +0700

    parameter horizontalAlignment

--- app/src/main/java/com/example/basiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/basiccomposable/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basiccomposable.ui.theme.BasicComposableTheme
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable // anotasi jetpack Compose
 fun BasicCompose(modifier: Modifier = Modifier){
-    Column (){  }
+    Column (horizontalAlignment = Alignment.CenterHorizontally){  }
 }
 
 @Composable
